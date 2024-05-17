b03e0c99da74dd544c57187bfe596dd807d6cac2
Divy Patel <82171007+divy-07@users.noreply.github.com>
2024-05-17 15:50:27 -0600

    added template files

---
+++ templates/pygame_move_wasd_template.py
@@ -0,0 +1,70 @@
+import sys
+import os
+import pygame
+
+"""
+SETUP section - preparing everything before the main loop runs
+"""
+pygame.init()
+
+# Global constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+FRAME_RATE = 60
+
+# Useful colors 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Creating the screen and the clock
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.set_alpha(0)  # Make alpha bits transparent
+clock = pygame.time.Clock()
+
+while True:
+    """
+    EVENTS section - how the code reacts when users do things
+    """
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # When user clicks the 'x' on the window, close our game
+            pygame.quit()
+            sys.exit()
+
+    # Keyboard events
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_w]:
+        pass  # Replace this line
+    if keys_pressed[pygame.K_a]:
+        pass  # Replace this line
+    if keys_pressed[pygame.K_d]:
+        pass  # Replace this line
+    if keys_pressed[pygame.K_s]:
+        pass  # Replace this line
+
+    # Mouse events
+    mouse_pos = pygame.mouse.get_pos()  # Get position of mouse as a tuple representing the
+    # (x, y) coordinate
+
+    mouse_buttons = pygame.mouse.get_pressed()
+    if mouse_buttons[0]:  # If left mouse pressed
+        pass  # Replace this line
+    if mouse_buttons[2]:  # If right mouse pressed
+        pass  # Replace this line
+
+
+
+    """
+    UPDATE section - manipulate everything on the screen
+    """
+    
+
+
+    """
+    DRAW section - make everything show up on screen
+    """
+    screen.fill(BLACK)  # Fill the screen with one colour
+
+
+
+    pygame.display.flip()  # Pygame uses a double-buffer, without this we see half-completed frames
+    clock.tick(FRAME_RATE)  # Pause the clock to always maintain FRAME_RATE frames per second
---
+++ templates/pygame_template.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import pygame
+
+"""
+SETUP section - preparing everything before the main loop runs
+"""
+pygame.init()
+
+# Global constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+FRAME_RATE = 60
+
+# Useful colors 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Creating the screen and the clock
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.set_alpha(0)  # Make alpha bits transparent
+clock = pygame.time.Clock()
+
+
+while True:
+    """
+    EVENTS section - how the code reacts when users do things
+    """
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # When user clicks the 'x' on the window, close our game
+            pygame.quit()
+            sys.exit()
+        
+
+
+    """
+    UPDATE section - manipulate everything on the screen
+    """
+
+    
+    
+
+
+    """
+    DRAW section - make everything show up on screen
+    """
+    screen.fill(BLACK)  # Fill the screen with one colour
+
+
+    pygame.display.flip()  # Pygame uses a double-buffer, without this we see half-completed frames
+    clock.tick(FRAME_RATE)  # Pause the clock to always maintain FRAME_RATE frames per second
+    
